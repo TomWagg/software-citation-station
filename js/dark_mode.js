d00fbfc530a83f167463daf4d9bff9449d149753
Tom Wagg <tomjwagg@gmail.com>
2024-04-19 18:09:04 -0400

    add helper css/js

---
+++ js/dark_mode.js
@@ -0,0 +1,16 @@
+this.window.addEventListener('load', function () {
+    document.querySelector('#dark-mode-checkbox').addEventListener('change', function () {
+        if (this.checked) {
+            document.body.classList.add('dark')
+            document.documentElement.setAttribute('data-bs-theme', 'dark')
+        } else {
+            document.body.classList.remove('dark')
+            document.documentElement.setAttribute('data-bs-theme', 'light')
+        }
+    })
+
+    let now = new Date()
+    if (now.getHours() >= 19 || now.getHours() < 6) {
+        document.querySelector('#dark-mode-checkbox').click()
+    }
+})
