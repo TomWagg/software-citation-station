7b415992d2e7bb3af2e9479d21d78fc60112f2fb
Tom Wagg <tomjwagg@gmail.com>
2024-04-21 20:55:16 -0400

    bibtex parsing, syntax highlighting

--- js/software.js
+++ js/software.js
@@ -1,16 +1,15 @@
-//const re = /"@\\w*{.*(?=\\,)"/g;
-const re = /@\w*{(?<tag>.*)(?=\,)/gmi;
+const bibtex_re = /@\w*{(?<tag>.*)(?=\,)/gmi;
+const latex_re = /(?<command>\\[^\\{]*)\{(?<refs>[^\}]*)\}/gmi;
 
 Promise.all([
     fetch('data/tags.json').then(x => x.json()),
     fetch('data/bibtex.bib').then(x => x.text())
 ]).then(([packages, bibtex_text]) => {
-    console.log(packages);
-    console.log(bibtex_text);
-    console.log(parse_bibtex(bibtex_text));
+    let bibtex_table = parse_bibtex(bibtex_text);
     const template_btn = document.getElementById('software-btn-template');
     const software_list = document.getElementById('software-list');
-    const acknowledgement = document.getElementById("acknowledgement");
+    const ack = document.getElementById("acknowledgement");
+    const bibtex_box = document.getElementById("bibtex");
     for (var key in packages) {
         const btn = template_btn.cloneNode(true);
 
@@ -21,15 +20,25 @@ Promise.all([
         
         btn.addEventListener('click', function() {
             this.classList.toggle("active");
-            acknowledgement.innerText = "";
+            ack.innerText = "";
+            bibtex_box.innerHTML = "";
+            let ack_to_add = [];
+            let bibtex_strings_to_add = [];
             document.querySelectorAll(".software-item.active").forEach(function(btn) {
-                acknowledgement.innerHTML = add_to_acknowledgement(
-                    acknowledgement.innerHTML,
-                    btn.querySelector(".software-name").innerText,
-                    packages[btn.getAttribute("data-key")]["tags"]
-                );
-                console.log(btn.getAttribute("data-key"));
+                let btn_tags = packages[btn.getAttribute("data-key")]["tags"];
+
+                let new_ack = "\\texttt{" + btn.querySelector(".software-name").innerText + "} \\citep{" + btn_tags.join(", ") + "}"
+                ack_to_add.push(new_ack.replace(latex_re, function(match, command, refs) {
+                    return '<span class="latex-command">' + command + '</span>{<span class="latex-refs">' + refs + "</span>}";
+                }));
+                for (let tag of btn_tags) {
+                    bibtex_strings_to_add.push(highlight_bibtex(bibtex_table[tag]));
+                }
             });
+
+            ack.innerHTML = "This work made use of the following software packages: "
+            ack.innerHTML += ack_to_add.slice(0, -1).join(', ') + (ack_to_add.length > 1 ? ' and ' : '') + ack_to_add.slice(-1) + '.';
+            bibtex_box.innerHTML = bibtex_strings_to_add.join("\n\n");
             console.log(packages[this.getAttribute("data-key")]["tags"]);
         });
 
@@ -38,43 +47,12 @@ Promise.all([
     }
 });
 
-
-function add_to_acknowledgement(ack, name, tags) {
-    if (ack === "") {
-        ack = "This work made use of the following software packages: ";
-    }
-    ack += "\\texttt{" + name + "} \\citep{" + tags.join(", ") + "}, ";
-    return ack
-}
-
 function parse_bibtex(bibtex_text) {
-    let tags = [];
-    let entries = [];
-
-    console.log(bibtex_text.match(re))
-    console.log([...bibtex_text.matchAll(re)])
-
-    while ((match = re.exec(bibtex_text)) != null) {
-        console.log("match found at " + match.index, match.groups["tag"]);
+    let bibtex_obj = {};
+    while ((match = bibtex_re.exec(bibtex_text)) != null) {
+        bibtex_obj[match.groups["tag"]] = isolate_bibtex_entry(bibtex_text, match.index);
     }
-
-    // console.log(bibtex_text.matchAll(re));
-
-    //do {
-    //    m = re.exec(bibtex_text);
-     //   if (m) {
-      //      console.log(m[1], m[2]);
-      //  }
-    //} while (m);
-    
-
-    /*for (let i = 0; i < lines.length; i++) {
-        if (line.startsWith("@")) {
-            let entry = isolate_bibtex_entry(lines, i);
-            entries.push(entry);
-            i += entry.split("\n").length;
-        }
-    }*/
+    return bibtex_obj
 }
 
 function isolate_bibtex_entry(s, start) {
@@ -85,11 +63,18 @@ function isolate_bibtex_entry(s, start) {
     while (braces > 0 || not_opened) {
         if (s[cursor] == "{") {
             braces += 1
-            not_opened = False
+            not_opened = false
         } else if (s[cursor] == "}") {
             braces -= 1
         }
         cursor += 1
     }
     return s.slice(start, cursor)
+}
+
+function highlight_bibtex(s) {
+    let at_thing = s.split("{")[0];
+    let key = s.split("{")[1].split(",")[0];
+    rest_starts_at = at_thing.length + key.length + 1;
+    return "<span class='bibtex-format'>" + at_thing + "</span>{<span class='bibtex-key'>" + key + "</span>" + s.slice(rest_starts_at);
 }
