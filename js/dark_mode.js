7d2dcfeba6f1c25956f53167f0905feb378cc99e
Tom Wagg <tomjwagg@gmail.com>
2024-05-24 21:52:43 -0700

    same colour logo

--- js/dark_mode.js
+++ js/dark_mode.js
@@ -2,11 +2,9 @@ document.querySelector('#dark-mode-checkbox').addEventListener('change', functio
     if (this.checked) {
         document.body.classList.add('dark')
         document.documentElement.setAttribute('data-bs-theme', 'dark')
-        document.getElementById('main-logo').src = 'img/software-citation-station-no-circle.png'
     } else {
         document.body.classList.remove('dark')
         document.documentElement.setAttribute('data-bs-theme', 'light')
-        document.getElementById('main-logo').src = 'img/software-citation-station-no-circle-blue.png'
     }
 })
 
