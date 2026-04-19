9038f951d7975d9dcbf825f3a48019a425ecc902
Andrea Zonca <code@andreazonca.com>
2026-04-19 14:04:51 -0700

    Implement types.ts

---
+++ src/cli/types.ts
@@ -0,0 +1,45 @@
+export interface CitationEntry {
+  tags: string[];
+  logo: string;
+  language: string[];
+  category: string[];
+  keywords: string[];
+  description: string;
+  link: string;
+  zenodo_doi: string;
+  attribution_link: string;
+  custom_citation: string;
+  dependencies: string[];
+  feature_tags?: Array<Record<string, string[]>>;
+  frequently_used?: boolean;
+  pypi_name?: string;
+  logo_background?: boolean;
+}
+
+export interface Citations {
+  [packageName: string]: CitationEntry;
+}
+
+export interface ZenodoVersion {
+  version: string;
+  doi: string;
+}
+
+export interface CachedVersionData {
+  fetchedAt: string;
+  versions: ZenodoVersion[];
+}
+
+export interface ParsedPackage {
+  name: string;
+  version?: string;
+  features?: string[];
+}
+
+export interface CitationOutput {
+  packages: string[];
+  timestamp: string;
+  dependencies: Record<string, string[]>;
+  acknowledgments: string;
+  bibtex: string;
+}
